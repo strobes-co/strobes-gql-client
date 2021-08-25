7bdafdf520e7e965d78b9dcacec81252b33c9b3d
Akhil Reni <36848653+akhil-reni@users.noreply.github.com>
2021-08-25 10:31:05 +0530

    Update example.py

--- examples/example.py
+++ examples/example.py
@@ -3,7 +3,7 @@ from strobes_gql_client.client import StrobesGQLClient
 
 s = StrobesGQLClient("a@wesecureapp.com", "a", "qa1.strobes.wsa-apps.com", 80, "http")
 
-list_bugs = s.op.all_bugs(organization_id="727f30bc-05fc-4d3e-aacb-e1fcd8ed65fc", search_query="ipaddress ~ \"1\"")
+list_bugs = s.get_op().all_bugs(organization_id="727f30bc-05fc-4d3e-aacb-e1fcd8ed65fc", search_query="ipaddress ~ \"1\"")
 
 data = s.endpoint(s.op)
 
