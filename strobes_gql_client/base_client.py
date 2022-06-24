f2087672b8f7b90e799e682629bcf87f5a5e11aa
Akhil Reni <akhil@wesecureapp.com>
2022-06-24 09:39:14 +0530

    fix: add an option to disable SSL verify

--- strobes_gql_client/base_client.py
+++ strobes_gql_client/base_client.py
@@ -7,7 +7,7 @@ from strobes_gql_client.helpers import get_jwt_token
 class BaseClient:
     def __init__(self, email: str = None, password: str = None,
                  host: str = enums.APP_HOST, port: int = enums.APP_PORT,
-                 scheme: str = enums.APP_SCHEME, api_token: str = None):
+                 scheme: str = enums.APP_SCHEME, api_token: str = None, verify: bool = True):
         self.app_url = f"{scheme}://{host}:" \
             f"{str(port)}/"
         if not api_token:
@@ -15,7 +15,7 @@ class BaseClient:
                 raise EmailException
             if not password:
                 raise PasswordException
-            token = get_jwt_token(self.app_url, email, password)
+            token = get_jwt_token(self.app_url, email, password, verify=verify)
             self.headers = {"Authorization": f"JWT {token}",
                                 "user-agent": enums.USER_AGENT}
         else:
--- strobes_gql_client/client.py
+++ strobes_gql_client/client.py
@@ -4,12 +4,16 @@ from sgqlc.types import Type, Field, list_of
 from sgqlc.types.relay import Connection, connection_args
 from sgqlc.operation import Operation
 from strobes_gql_client.schema import schema
+import requests
 
 class StrobesGQLClient(BaseClient):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        session = requests.Session()
+        session.verify = kwargs.get('verify', True)
+        print(session.verify)
         self.grapqhl_url = f'{self.app_url}api/graphql/'
-        self.endpoint = RequestsEndpoint(self.grapqhl_url, self.headers)
+        self.endpoint = RequestsEndpoint(self.grapqhl_url, self.headers, session=session)
     
     def get_op(self):
         self.op = Operation(schema.Query)
