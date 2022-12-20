ffc51c0491bd00941f7de7f3bba918119438d427
Danish Ansari <danish@wesecureapp.com>
2022-12-20 11:09:14 +0530

    feat(bug-cursor-paginated-type): Added BugCursorPaginatedType and removed BugPaginatedType

--- strobes_gql_client/schema.py
+++ strobes_gql_client/schema.py
@@ -99,16 +99,13 @@ class AssetType(sgqlc.types.Type):
     mac_address = sgqlc.types.Field(String, graphql_name='macAddress')
     os = sgqlc.types.Field(String, graphql_name='os')
 
-
-class BugPaginatedType(sgqlc.types.Type):
+class BugCursorPaginatedType(sgqlc.types.Type):
     __schema__ = schema
-    __field_names__ = ('page', 'total_pages', 'page_size', 'total_count', 'has_next', 'has_prev', 'objects')
-    page = sgqlc.types.Field(Int, graphql_name='page')
-    total_pages = sgqlc.types.Field(Int, graphql_name='totalPages')
-    page_size = sgqlc.types.Field(Int, graphql_name='pageSize')
-    total_count = sgqlc.types.Field(Int, graphql_name='totalCount')
+    __field_names__ = ()
     has_next = sgqlc.types.Field(Boolean, graphql_name='hasNext')
-    has_prev = sgqlc.types.Field(Boolean, graphql_name='hasPrev')
+    has_previous = sgqlc.types.Field(Boolean, graphql_name='hasPrevious')
+    last_cursor = sgqlc.types.Field(String, graphql_name='lastCursor')
+    before_cursor = sgqlc.types.Field(String, graphql_name='beforeCursor')
     objects = sgqlc.types.Field(sgqlc.types.list_of('BugType'), graphql_name='objects')
 
 
@@ -537,13 +534,15 @@ class Query(sgqlc.types.Type):
         ('export_report_type', sgqlc.types.Arg(String, graphql_name='exportReportType', default=None)),
 ))
     )
-    all_bugs = sgqlc.types.Field(BugPaginatedType, graphql_name='allBugs', args=sgqlc.types.ArgDict((
+    all_bugs = sgqlc.types.Field(BugCursorPaginatedType, graphql_name='allBugs', args=sgqlc.types.ArgDict((
         ('organization_id', sgqlc.types.Arg(UUID, graphql_name='organizationId', default=None)),
         ('search_query', sgqlc.types.Arg(String, graphql_name='searchQuery', default=None)),
         ('order_by', sgqlc.types.Arg(sgqlc.types.list_of(String), graphql_name='orderBy', default=None)),
         ('page', sgqlc.types.Arg(Int, graphql_name='page', default=None)),
         ('page_size', sgqlc.types.Arg(Int, graphql_name='pageSize', default=None)),
         ('export_report_type', sgqlc.types.Arg(String, graphql_name='exportReportType', default=None)),
+        ('after', sgqlc.types.Arg(String, graphql_name='after', default=None)),
+        ('before', sgqlc.types.Arg(String, graphql_name='before', default=None)),
 ))
     )
 
