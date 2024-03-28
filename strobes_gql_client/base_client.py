1b1cba0790627e9c3eddb9f477938c998ef0de3c
avinashbakshi01 <avinash@wesecureapp.com>
2024-03-28 14:57:40 +0530

    feat:added support to api key authentication

--- strobes_gql_client/base_client.py
+++ strobes_gql_client/base_client.py
@@ -1,23 +1,18 @@
 import requests
 from strobes_gql_client import enums
-from strobes_gql_client.exceptions import EmailException, PasswordException
-from strobes_gql_client.helpers import get_jwt_token
-
+from strobes_gql_client.exceptions import AuthenticationFailure
 
 class BaseClient:
-    def __init__(self, email: str = None, password: str = None,
-                 host: str = enums.APP_HOST, port: int = enums.APP_PORT,
-                 scheme: str = enums.APP_SCHEME, api_token: str = None, verify: bool = True):
-        self.app_url = f"{scheme}://{host}:" \
-            f"{str(port)}/"
-        if not api_token:
-            if not email:
-                raise EmailException
-            if not password:
-                raise PasswordException
-            token = get_jwt_token(self.app_url, email, password, verify=verify)
-            self.headers = {"Authorization": f"JWT {token}",
-                                "user-agent": enums.USER_AGENT}
-        else:
-            self.headers = {"Authorization": f"{api_token}",
-                                "user-agent": enums.USER_AGENT}
+    def __init__(
+        self,
+        host: str = enums.APP_HOST,
+        port: int = enums.APP_PORT,
+        scheme: str = enums.APP_SCHEME,
+        api_token: str = None,
+        verify: bool = True,
+    ):
+        self.app_url = f"{scheme}://{host}:" f"{str(port)}/"
+        self.headers = {
+                    "Authorization": f"token {api_token}",
+                    "user-agent": enums.USER_AGENT,
+                }
