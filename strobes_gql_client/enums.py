cf88489e847065539c7f62add23976ae14fd76b9
swakhil-strobes <swakhil.mattapalli@strobes.co>
2025-08-01 16:42:14 +0530

    ci: add PR test runner with secure tokens

--- strobes_gql_client/enums.py
+++ strobes_gql_client/enums.py
@@ -1,8 +1,10 @@
+import os
+
 USER_AGENT = 'strobes-python-gql-client'
 APP_PORT = 443
 APP_SCHEME = "https"
 # Copy of the APP_HOST API token and organization ID from the Strobes web application.
 APP_HOST = "automate.in.strobes.co"
-API_TOKEN = "ef0276eafeba8253373b0ccdbfe97876473b23f9"
-ORGANIZATION_ID = "23604f83-f44d-44c7-8b9f-00f08bfab8eb"
+API_TOKEN = os.getenv('STROBES_API_TOKEN')
+ORGANIZATION_ID = os.getenv('STROBES_ORGANIZATION_ID')
 
